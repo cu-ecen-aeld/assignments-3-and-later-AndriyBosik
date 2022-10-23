c2d1139d30a92854f3c798d775bd852becb0576b
andriibosyk <andriybosik2002@ukr.net>
2022-10-23 15:55:25 +0300

    Minor fix

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 files_dir=$1
 search_str=$2
+
 if [ -z $files_dir ] || [ -z $search_str ]
 then
     echo "Some parameters were not specified"
     exit 1
 fi
+
 if [ ! -d $files_dir ]
 then
     echo "$files_dir is not a directory";
     exit 1
 fi
+
 files_count=$(find $files_dir -type f | wc -l)
 files=$(find $files_dir -type f)
 lines_count=0
